8ab5939af74406486866e8a1df2b37d09d448bd0
Malinda <salgaduwml@gmail.com>
2023-10-13 15:44:42 +0530

    Complete project

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
-# React + Vite
+# Password Generator React js
+
+![Alt text](image.png)
 
 This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
 
--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,100 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+/* eslint-disable no-unused-vars */
+import { useState, useCallback, useEffect, useRef } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [length, setLength] = useState(8);
+  const [numberAllowed, setNumberAllowed] = useState(false);
+  const [charAllowed, setCharAllowed] = useState(false);
+  const [password, setPassword] = useState("");
+
+  const passwordRef = useRef(null);
+
+  const generatePassword = useCallback(() => {
+    let pass = "";
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    if (numberAllowed) str += "0123456789";
+    if (charAllowed) str += "!@#$%^&*()_+";
+
+    for (let i = 1; i <= length; i++) {
+      const char = Math.floor(Math.random() * str.length + 1);
+      pass += str.charAt(char);
+    }
+    setPassword(pass);
+  }, [length, numberAllowed, charAllowed]);
+
+  useEffect(() => {
+    generatePassword();
+  }, [length, numberAllowed, charAllowed]);
+
+  const copyPasswordToClipboard = () => {
+    window.navigator.clipboard.writeText(password);
+    passwordRef.current?.select();
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+    <div className="w-screen h-screen flex justify-center items-center">
+      <div className="w-full bg-slate-800 max-w-lg p-6 border border-[rgba(255,255,255,0.2)] rounded-md">
+        <h1 className="text-white font-semibold text-3xl">
+          Password Generator
+        </h1>
+        <div className="mt-10 flex items-stretch overflow-hidden rounded-md">
+          <input
+            type="text"
+            className="w-full p-2 outline-none text-black"
+            value={password}
+            readOnly
+            ref={passwordRef}
+          />
+          <button
+            className="bg-blue-600 px-3 h-auto text-white"
+            onClick={copyPasswordToClipboard}
+          >
+            Copy
+          </button>
+        </div>
+        <div className="flex items-center justify-between gap-4 mt-6">
+          <div className="flex items-center text-white gap-4">
+            <input
+              type="range"
+              min={6}
+              max={20}
+              value={length}
+              onChange={(e) => setLength(e.target.value)}
+              className="cursor-pointer"
+            />
+            <label htmlFor="label">Length: {length}</label>
+          </div>
+          <div className="flex items-center text-white gap-x-1">
+            <input
+              type="checkbox"
+              defaultChecked={numberAllowed}
+              onChange={() => {
+                setNumberAllowed((prev) => !prev);
+              }}
+              id="number"
+            />
+            <label htmlFor="number">Numbers</label>
+          </div>
+          <div className="flex items-center text-white gap-x-1">
+            <input
+              type="checkbox"
+              defaultChecked={charAllowed}
+              onChange={() => {
+                setCharAllowed((prev) => !prev);
+              }}
+              id="character"
+            />
+            <label htmlFor="character">Characters</label>
+          </div>
+        </div>
+        <p className="text-sm mt-10 text-center text-gray-400">
+          Made with ❤️ by Malinda Lakshan
+          <br />
+          www.malindalakshan.com
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
